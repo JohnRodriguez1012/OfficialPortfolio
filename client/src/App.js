ad2e45841a84880361f67988751b3081280e8f46
John Rodriguez <johnnyrod92@gmail.com>
2018-06-20 15:25:28 -0400

    cleaned code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import NavTabs from "./Components/Navbar/NavTabs.js";
 import Landing from "./Components/Landing/Landing.js";
--- client/src/Components/Navbar/NavTabs.js
+++ client/src/Components/Navbar/NavTabs.js
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import {Navbar, NavItem, Nav} from "react-bootstrap"
 
 
